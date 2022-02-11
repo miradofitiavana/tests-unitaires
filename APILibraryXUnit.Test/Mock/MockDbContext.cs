83ae8721f814173799eb1914c3793ab00d73fb88
miradofitiavana <miradofitiavana@gmail.com>
2022-02-11 15:10:03 +0100

    db connext mock

---
+++ APILibraryXUnit.Test/Mock/MockDbContext.cs
@@ -0,0 +1,50 @@
+﻿using APILibrary.Test.Mock.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+
+namespace APILibrary.Test.Mock
+{
+    public class MockDbContext : EatDbContext
+    {
+        public MockDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public static MockDbContext GetDbContext(bool withData = true)
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("dbtest").Options;
+            var db = new MockDbContext(options);
+
+            if (withData)
+            {
+                db.Pizzas.Add(new PizzaMock
+                {
+                    Name = "Pizza 4 fromages",
+                    Price = 12,
+                    Topping = "4 fromages",
+                    DateCreation = new System.DateTime()
+                });
+
+                db.Pizzas.Add(new PizzaMock
+                {
+                    Name = "Pizza 2",
+                    Price = 11,
+                    Topping = "Pizza 12 12",
+                    DateCreation = new System.DateTime()
+                });
+
+                db.Pizzas.Add(new PizzaMock
+                {
+                    Name = "Pizza 3",
+                    Price = 15,
+                    Topping = "Pizza 3 3 3",
+                    DateCreation = new System.DateTime()
+                });
+
+                db.SaveChanges();
+            }
+
+            return db;
+        }
+    }
+}
--- APILibraryXUnit.Test/PizzaTest.cs
+++ APILibraryXUnit.Test/PizzaTest.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebApplication.Models;
+using Xunit;
 
 namespace APILibraryXUnit.Test
 {
-    class PizzaTest
+    [Trait("creation d'une commande", "")]
+    public class PizzaTest
     {
+        [Fact(DisplayName = "je peux creer une commande")]
+        public void Je_peux_creer_une_commande()
+        {
+            //var pizza = new Pizza();
+
+            //var result = Order.Create(customer, pizza);
+
+            //result.IsSuccess.Should().BeTrue();
+        }
     }
 }
